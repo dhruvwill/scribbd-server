bce82d5621a4c9ce6b474bbe4ada44eeb2d7b837
Dhruvwill <ddyhs25@gmail.com>
2024-04-17 19:14:38 +0530

    Updated Schema, Synced scoket.io rooms with mongodb collection. User can join and leave room

--- controllers/Room.js
+++ controllers/Room.js
@@ -1,6 +1,5 @@
 const prisma = require("../prisma/index");
-const { io } = require("../index");
-async function joinRandomRoom(socketId) {
+async function joinRandomRoom(socketId, userData) {
   const rooms = await prisma.room.findMany();
   if (rooms.length === 0) {
     const room = await prisma.room.create({
@@ -13,6 +12,7 @@ async function joinRandomRoom(socketId) {
           {
             socketID: socketId,
             score: 0,
+            character: userData,
           },
         ],
         roomId: "room1",
@@ -32,6 +32,7 @@ async function joinRandomRoom(socketId) {
             {
               socketID: socketId,
               score: 0,
+              character: userData,
             },
           ],
         },
@@ -48,58 +49,96 @@ async function joinRandomRoom(socketId) {
             {
               socketID: socketId,
               score: 0,
+              character: userData,
             },
           ],
-          roomId: rooms.length + 1,
+          roomId: `room${rooms.length + 1}`,
         },
       });
       return room;
     }
   }
 }
-async function leaveRoom(socketId) {
-  // Retrieve all rooms where this socketId is present in the users array
-  const roomsWithUser = await prisma.room.findMany({
+async function leaveRoom(socket) {
+  const room = await prisma.room.findFirst({
     where: {
       users: {
         some: {
-          socketID: socketId,
+          socketID: socket.id,
         },
       },
     },
   });
 
-  // Iterate over each room and remove the user from the users array
-  await Promise.all(
-    roomsWithUser.map(async (room) => {
-      const updatedUsers = room.users.filter(
-        (user) => user.socketID !== socketId
-      );
+  if (!room) {
+    return;
+  } else {
+    const updatedUsers = room.users.filter(
+      (user) => user.socketID !== socket.id
+    );
 
-      // Update the room with the filtered users array
-      await prisma.room.update({
+    await prisma.room.update({
+      where: {
+        id: room.id,
+      },
+      data: {
+        users: updatedUsers,
+      },
+    });
+
+    socket.leave(room.roomId);
+
+    if (updatedUsers.length === 0) {
+      await prisma.room.delete({
         where: {
           id: room.id,
         },
-        data: {
-          users: updatedUsers,
-        },
       });
-
-      // Optional: If no users are left in the room, handle the room accordingly (delete, set inactive, etc.)
-      if (updatedUsers.length === 0) {
-        // Example action: delete the room
-        await prisma.room.delete({
-          where: {
-            id: room.id,
-          },
-        });
-      }
-    })
-  );
-
-  // Optionally, you might want to return some status or result here, depending on your application's needs
-  return { message: "User removed from all rooms successfully." };
+    }
+  }
 }
 
 module.exports = { joinRandomRoom, leaveRoom };
+
+// // Retrieve all rooms where this socketId is present in the users array
+// const roomsWithUser = await prisma.room.findMany({
+//   where: {
+//     users: {
+//       some: {
+//         socketID: socketId,
+//       },
+//     },
+//   },
+// });
+
+// // Iterate over each room and remove the user from the users array
+// await Promise.all(
+//   roomsWithUser.map(async (room) => {
+//     const updatedUsers = room.users.filter(
+//       (user) => user.socketID !== socketId
+//     );
+
+//     // Update the room with the filtered users array
+//     await prisma.room.update({
+//       where: {
+//         id: room.id,
+//       },
+//       data: {
+//         users: updatedUsers,
+//       },
+//     });
+
+//     // Optional: If no users are left in the room, handle the room accordingly (delete, set inactive, etc.)
+//     if (updatedUsers.length === 0) {
+//       // Example action: delete the room
+//       await prisma.room.delete({
+//         where: {
+//           id: room.id,
+//         },
+//       });
+//     }
+//   })
+// );
+
+// // Optionally, you might want to return some status or result here, depending on your application's needs
+// return { message: "User removed from all rooms successfully." };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { createServer } = require("node:http");
-const { join } = require("node:path");
 const { Server } = require("socket.io");
 const { instrument } = require("@socket.io/admin-ui");
 const cors = require("cors");
 const { joinRandomRoom, leaveRoom } = require("./controllers/Room");
+const { SocketAddress } = require("node:net");
 
 const app = express();
 const server = createServer(app);
@@ -23,15 +23,26 @@ instrument(io, {
 io.on("connection", (socket) => {
   console.log("connected: ", socket.id, io.engine.clientsCount);
 
-  socket.on("join", async () => {
+  socket.on("join", async (userData) => {
     console.log("join: ", socket.id);
-    await joinRandomRoom(socket.id);
+    console.log(userData);
+    const room = await joinRandomRoom(socket.id, userData);
+    socket.join(room.roomId);
+  });
+
+  socket.on("leave", async () => {
+    console.log("leave: ", socket.id);
+    const room = await leaveRoom(socket);
   });
 
   socket.on("disconnect", async () => {
+    await leaveRoom(socket);
     console.log("disconnected: ", socket.id, io.engine.clientsCount);
-    await leaveRoom(socket.id);
   });
+});
+
+io.of("/").adapter.on("join-room", (room, id) => {
+  console.log(`socket ${io.sockets.sockets.get(id)} has joined room ${room}`);
 });
 
 server.listen(PORT, () => {
